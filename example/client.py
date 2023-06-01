58876ccdac1471c7746654add466918b7d3e3425
cutecutecat <12032261@mail.sustech.edu.cn>
2023-06-01 12:10:01 +0800

    feat: support llmspec format
    Signed-off-by: cutecutecat <starkind1997@gmail.com>

--- example/client.py
+++ example/client.py
@@ -15,27 +15,56 @@ with open("dog_video.mp4", "rb") as f:
 REMOTE_URL = "https://xxx.modelz.tech/"
 TOKEN = "mzi-ttt"
 
+
 class Input(TypedDict):
-    task: Literal["image", "audio", "video", "text"]
-    data: List[bytes | str]
+    input: List[bytes | str]
+    model: Literal[
+        "imagebind-image",
+        "imagebind-audio",
+        "imagebind-video",
+        "imagebind-text",
+    ]
+
+
+class Embedding(TypedDict):
+    embedding: List[float]
+    index: int
+    object: Literal["embedding"]
+
+
+class Output(TypedDict):
+    data: List[Embedding]
+    model: Literal[
+        "imagebind-image",
+        "imagebind-audio",
+        "imagebind-video",
+        "imagebind-text",
+    ]
+    object: Literal["list"]
+
 
 cases: List[Input] = [
-    {"task":"text", "data": ["A dog", "doggery", "puppy"]},
-    {"task":"image", "data":[dog_image_bytes]},
-    {"task":"audio", "data":[dog_audio_bytes]},
-    {"task":"video", "data":[dog_video_bytes]},
+    {"model": "imagebind-text", "input": ["A dog", "doggery", "puppy"]},
+    {"model": "imagebind-image", "input": [dog_image_bytes]},
+    {"model": "imagebind-audio", "input": [dog_audio_bytes]},
+    {"model": "imagebind-video", "input": [dog_video_bytes]},
 ]
 
 for case in cases:
+    print("case for task {}:".format(case["model"]))
     prediction = httpx.post(
         f"{REMOTE_URL}/inference",
         headers={"X-API-Key": TOKEN},
         data=msgpack.packb(case),
-        timeout=httpx.Timeout(timeout=60.0)
+        timeout=httpx.Timeout(timeout=60.0),
     )
     if prediction.status_code == 200:
-        data = msgpack.unpackb(prediction.content)
-        print(f'{str(data)[:50]}...{str(data)[-50:]}')
+        output: Output = msgpack.unpackb(prediction.content)
+        embeddings = output["data"]
+        for emb in embeddings:
+            print(
+                f"{emb['index']}-{str(emb['embedding'])[:50]}...{str(emb['embedding'])[-50:]}"
+            )
     else:
         print(prediction.status_code)
         print(prediction.content)
